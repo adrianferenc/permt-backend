03beaa5010b916ec96c2c2bbd18fe1e8d3c98eee
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2022-08-03 21:52:08 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const permitControllers = require('./src/utilities');
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/test', function (req, res) {
+    console.log('someone has hit the /test endpoint');
+    permitControllers.viewPermits().then((permitList) => {
+        res.status(200).set('Content-Type', 'text/html').send(Buffer.from(permitList));
+    });
+});
+
+app.post('/', function (req, res) {
+    console.log('someone has made a post request');
+    console.log(req.body);
+    if ('plateNo' in req.body) {
+        permitControllers.buyAPermit(req.body.plateNo).then((success) => {
+            if (success) {
+                res.status(200).json({ working: true });
+            } else {
+                res.status(400).json({ working: false });
+            }
+        });
+    } else {
+        res.status(401).json({ working: false });
+    }
+});
+
+// Listen on port 4000
+app.listen(port, function () {
+    console.log(`Listening on port: ${port}`);
+});
---
+++ src/utilities.js
@@ -0,0 +1,249 @@
+const axios = require('axios');
+
+module.exports = {
+    buyAPermit,
+    viewPermits,
+};
+
+async function customerAccessLoginAction() {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'https://prdwmq.etimspayments.com/pbw/customerAccessLoginAction.doh',
+            data: `clientcode=1W&clientAccount=5&requestType=signin&loginAttemptCount=0&parking=&processType=RPP&userName=adrianferenc%40gmail.com&password=${process.env.PW}&signin=Sign+In`,
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                dnt: 1,
+                origin: 'https://prdwmq.etimspayments.com',
+                referer:
+                    'https://prdwmq.etimspayments.com/pbw/dispatchPageAction.doh?clientcode=1W&requestType=logOff&processType=RPP',
+                'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"`,
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': `"macOS"`,
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': 1,
+            },
+        });
+        const JSESSIONID = res.headers['set-cookie'][0].split(';')[0];
+        const visid_incap_2087381 = res.headers['set-cookie'][1].split(';')[0];
+        const incap_ses_32_2087381 = res.headers['set-cookie'][2].split(';')[0];
+        const cookie = `${visid_incap_2087381}; ${incap_ses_32_2087381}; ${JSESSIONID}`;
+        return cookie;
+    } catch (e) {
+        console.log('customerAccessLoginAction');
+    }
+}
+
+async function accountSummaryRequestAction(cookie) {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'https://prdwmq.etimspayments.com/pbw/accountSummaryRequestAction.doh',
+            data: `clientcode=1W&clientAccount=5&userName=adrianferenc%40gmail.com&requestType=permitaccountdetails&processType=RPP`,
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                cookie,
+                dnt: 1,
+                origin: 'https://prdwmq.etimspayments.com',
+                referer: 'https://prdwmq.etimspayments.com/pbw/customerAccessLoginAction.doh',
+                'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"`,
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': `"macOS"`,
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': 1,
+            },
+        });
+        return res;
+    } catch (e) {
+        console.log('accountSummaryRequestAction');
+    }
+}
+
+async function accountSummaryAction(action, cookie) {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'https://prdwmq.etimspayments.com/pbw/accountSummaryAction.doh',
+            data: `clientcode=1W&clientAccount=5&userName=adrianferenc%40gmail.com&requestType=${action}&processType=RPP`,
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                cookie,
+                dnt: 1,
+                origin: 'https://prdwmq.etimspayments.com',
+                referer: 'https://prdwmq.etimspayments.com/pbw/accountSummaryRequestAction.doh',
+                'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"`,
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': `"macOS"`,
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': 1,
+            },
+        });
+        return res;
+    } catch (e) {
+        console.log('accountSummaryRequestAction');
+    }
+}
+
+function dateFormatter(date) {
+    const month =
+        date.getMonth() < 9
+            ? '0' + (date.getMonth() + 1).toString()
+            : (date.getMonth() + 1).toString();
+    const day = date.getDate() < 10 ? '0' + date.getDate().toString() : date.getDate().toString();
+
+    const year = date.getFullYear().toString();
+
+    return `${month}%2F${day}%2F${year}`;
+}
+
+async function rppPermitOrderAction(action, cookie) {
+    try {
+        const today = new Date();
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+
+        const res = await axios({
+            method: 'POST',
+            url: 'https://prdwmq.etimspayments.com/pbw/rppPermitOrderAction.doh',
+            data: `clientcode=1W&clientAccount=5&requestType=${action}&permitType=VV&actionType=RPP&quantity=1&duration=1&startDate=${dateFormatter(
+                today
+            )}&endDate=${dateFormatter(tomorrow)}`,
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                cookie,
+                dnt: 1,
+                origin: 'https://prdwmq.etimspayments.com',
+                referer: 'https://prdwmq.etimspayments.com/pbw/accountSummaryAction.doh',
+                'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"`,
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': `"macOS"`,
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': 1,
+            },
+        });
+        return res;
+    } catch (e) {
+        console.log('rppPermitOrderAction');
+    }
+}
+
+async function rppPermitOrderVerifyAction(cookie) {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'https://prdwmq.etimspayments.com/pbw/rppPermitOrderVerifyAction.doh',
+            data: `clientcode=1W&clientAccount=5&requestType=submit&actionType=RPP`,
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                cookie,
+                dnt: 1,
+                origin: 'https://prdwmq.etimspayments.com',
+                referer: 'https://prdwmq.etimspayments.com/pbw/rppPermitOrderAction.dohs',
+                'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"`,
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': `"macOS"`,
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': 1,
+            },
+        });
+        return res;
+    } catch (e) {
+        console.log('accountSummaryRequestAction');
+    }
+}
+
+async function addPlate(permitNumber, plateNumber, cookie) {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'https://prdwmq.etimspayments.com/pbw/accountSummaryAction.doh',
+            data: `clientcode=1W&clientAccount=5&userName=adrianferenc%40gmail.com&requestType=permitUpdate&processType=RPP&accountNo=050329&lastName=ADRIAN+FERENC%2FCOREY+FETZER&permit=${permitNumber}&select=update&plateState=CA&newPlate=${plateNumber}&confirmPlate=${plateNumber}`,
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                cookie,
+                dnt: 1,
+                origin: 'https://prdwmq.etimspayments.com',
+                referer: 'https://prdwmq.etimspayments.com/pbw/accountSummaryRequestAction.doh',
+                'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"`,
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': `"macOS"`,
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': 1,
+            },
+        });
+        return res;
+    } catch (e) {
+        console.log('accountSummaryRequestAction');
+    }
+}
+
+async function buyAPermit(plateNo) {
+    //This logs in
+    const cookie = await customerAccessLoginAction();
+    await accountSummaryRequestAction(cookie);
+    await accountSummaryAction('permitSummary0', cookie);
+
+    //This adds a permit to the cart
+    await rppPermitOrderAction('addtocart', cookie);
+    await rppPermitOrderAction('checkout', cookie);
+
+    //This gets a new permit
+    await rppPermitOrderVerifyAction(cookie);
+
+    //This gets the name of the most recently obtained permit
+    const res = await accountSummaryAction('permitSummary0', cookie);
+    const start = res.data.lastIndexOf(`22V`);
+    const permitNumber = res.data.slice(start, start + 9);
+
+    //This updates the plate of the most recent permit to plateNo
+    await addPlate(permitNumber, plateNo, cookie);
+
+    //This checks that plateNo has been added
+    await accountSummaryRequestAction(cookie);
+    const res2 = await accountSummaryAction('permitSummary0', cookie);
+    console.log(res2.data.includes(plateNo.toUpperCase()));
+    return res2.data.includes(plateNo.toUpperCase());
+}
+
+async function viewPermits() {
+    const cookie = await customerAccessLoginAction();
+    await accountSummaryRequestAction(cookie);
+    const res = await accountSummaryAction('permitSummary0', cookie);
+    return res.data;
+}
